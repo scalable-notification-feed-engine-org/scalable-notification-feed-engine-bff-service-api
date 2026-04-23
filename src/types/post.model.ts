55a3cca84940f5b2c525756dcfdd4eebd2a6f2c2
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-23 17:23:39 +0530

    feat(gateway): add Post GraphQL model

---
+++ src/types/post.model.ts
@@ -0,0 +1,26 @@
+import { Field, ID, ObjectType, Int } from '@nestjs/graphql';
+import { User } from './user.model';
+
+@ObjectType()
+export class Post {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  content: string;
+
+  @Field(() => [String])
+  mediaUrls: string[];
+
+  @Field()
+  createAt: Date;
+
+  @Field(() => Int)
+  likeCount: number;
+
+  @Field(() => Int)
+  commentCount: number;
+
+  @Field(() => User)
+  author: User;
+}
