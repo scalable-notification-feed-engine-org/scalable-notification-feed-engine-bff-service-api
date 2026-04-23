d2bbc7ef31b9b9bb68d8d081cd0943bd0b5f24f2
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-23 17:21:26 +0530

    feat(app): bootstrap GraphQL gateway with Apollo, FeedService, and UserService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PostResolver } from './resolver/post.resolver';
+import { FeedService } from './service/feed.service';
+import { UserService } from './service/user.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      context: ({ req }) => ({ headers: req.headers }),
+    }),
+  ],
+  providers: [PostResolver,FeedService, UserService]
 })
 export class AppModule {}
