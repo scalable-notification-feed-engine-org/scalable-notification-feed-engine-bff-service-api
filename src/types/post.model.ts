12f002b18238a0e8772c3b44570c0f5d7a3748b5
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-28 14:19:37 +0530

    feat(gateway): add PostComment type and comments field to Post model

--- src/types/post.model.ts
+++ src/types/post.model.ts
@@ -1,6 +1,19 @@
 import { Field, ID, ObjectType, Int } from '@nestjs/graphql';
 import { User } from './user.model';
 
+
+@ObjectType()
+export class PostComment {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  content: string;
+
+  @Field()
+  userId: string;
+}
+
 @ObjectType()
 export class Post {
   @Field(() => ID)
@@ -23,4 +36,8 @@ export class Post {
 
   @Field(() => User)
   author: User;
+
+  @Field(() => [PostComment], { nullable: true })
+  comments?: PostComment[];
 }
+
